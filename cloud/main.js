8b9cd010a9a9e90d8992fc41ae1ee88196ccb6ae
Hunty Dumpty <hunter.horsley@gmail.com>
2014-09-13 23:37:59 -0400

    added usersNearbyNow query which finds all users who are within 33ft and who opened the app within the last 30 seconds

--- cloud/main.js
+++ cloud/main.js
@@ -6,26 +6,36 @@ Parse.Cloud.define("hello", function(request, response) {
 });
 
 
-Parse.Cloud.define("usersNearby", function(request, response) {
-    // request.user.location
-
-    var userLocation = new Parse.Query("UserLocations");
-    userLocation.equalTo("user", request.user);
-    userLocation.descending("currentTimeStamp");
-				userLocation.find({
-						success: function(locations) {
-							var query = new Parse.Query("UserLocations");
-    			query.withinKilometers("location", locations[0].get("location"), 0.01); // ~33ft
-    			var date = new Date();
-    			query.greaterThan("currentTimeStamp", date.getTime()/1000-30) // within last 30 sec
-    			query.find({
-    				success: function(locationsNearbyNow) {
-    					response.success(locationsNearbyNow);
-    				}
-    			})
+Parse.Cloud.define("usersNearbyNow", function(request, response) {
+ var userLocation = new Parse.Query("UserLocations");
+ userLocation.equalTo("user", request.user);
+ userLocation.descending("currentTimeStamp");
+	userLocation.find({
+		success: function(locations) {
+			var query = new Parse.Query("UserLocations");
+			query.withinKilometers("location", locations[0].get("location"), 0.01); // ~33ft
+			var date = new Date();
+			query.greaterThan("currentTimeStamp", date.getTime()/1000-30) // within last 30 sec
+			query.find({
+				success: function(locationsNearbyNow) {
+					var finalQuery = new Parse.Query("User");
+					finalQuery.equalTo("objectId", locationsNearbyNow.get("user"))
+					finalQuery.find({
+						success: function(usersNearbyNow) {
+							response.success(usersNearbyNow);
 						},
 						error: function(error) {
-							console.error( "Error" + error.code + ":" + error.message );
-						}
-
-				})
+						console.error( "Error" + error.code + ":" + error.message );
+						} 
+					});
+				},
+				error: function(error) {
+				console.error( "Error" + error.code + ":" + error.message );
+				}
+			});
+		},
+		error: function(error) {
+		console.error( "Error" + error.code + ":" + error.message );
+		}
+	});
+});

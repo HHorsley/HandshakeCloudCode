e06a6a26329dad4e0189d0eb5c59885cf5ee5a7c
Hunty Dumpty <hunter.horsley@gmail.com>
2014-09-13 23:19:42 -0400

    added usersNearbyNow query which finds all users who are within 33ft and who opened the app within the last 30 seconds

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,28 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+Parse.Cloud.define("usersNearby", function(request, response) {
+    // request.user.location
+
+    var userLocation = new Parse.Query("UserLocations");
+    userLocation.equalTo("user", request.user);
+    userLocation.descending("currentTimeStamp");
+				userLocation.find({
+						success: function(locations) {
+							var query = new Parse.Query("UserLocations");
+    			query.withinKilometers("location", locations[0].get("location"), 0.01); // ~33ft
+    			var date = new Date();
+    			query.greaterThan("currentTimeStamp", date.getTime()/1000-30) // within last 30 sec
+    			query.find({
+    				success: function(locationsNearbyNow) {
+    					response.success(locationsNearbyNow);
+    				}
+    			})
+						},
+						error: function(error) {
+							console.error( "Error" + error.code + ":" + error.message );
+						}
+
+				})
